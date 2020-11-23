665263cb1923c72e2a2ef0b141ec476e168787ab
girish004 <techygirish@gmail.com>
2020-11-23 20:42:59 +0530

    Almost complete game

--- game.py
+++ game.py
@@ -1,27 +1,88 @@
 import pygame
 from pygame.locals import *
+import time
+import random
 pygame.init()
-def show(nature,grass):
+def show(nature,grass,count):
+    green = (0, 255, 0)
+    blue = (0, 0, 128)
     screen.blit(grass, (0, 0))
     screen.blit(nature, ((0, 0)))
     screen.blit(nature, ((0, 200)))
     screen.blit(nature, ((0, 400)))
+    font = pygame.font.SysFont('freesansbold.ttf', 32)
+    score = font.render("Score: "+str(count),True,green,blue)
+    screen.blit(score,(1000,0))
+start_time=int(time.time())
+c=int(time.time())
+d=int(time.time())
 width, height = 1240, 620
 screen=pygame.display.set_mode((width, height))
 pos=[250,0]
+enemypos=[]
+timeline=[]
+green = (0, 255, 0)
+blue = (0, 0, 128)
+white = (255, 255, 255)
 player = pygame.image.load("D:\Girish\Project\Game\Resources\zombie.png")
 grass = pygame.image.load("D:\Girish\Project\Game\Resources\grass.jpg")
-hero = pygame.image.load("D:\Girish\Project\Game\Resources\hero.png")
+hero = pygame.image.load("D:\Girish\Project\Game\Resources\hero.png").convert_alpha()
 nature = pygame.image.load("D:\Girish\Project\Game\Resources\safe.png")
+bullet = pygame.image.load("D:\Girish\Project\Game\Resources\Bullet.png")
+pygame.display.set_caption('Show Text')
+font = pygame.font.SysFont('freesansbold.ttf', 32)
+text=font.render("Zombie appocalypse",True,blue,green)
+textRect = text.get_rect()
 player = pygame.transform.scale(player, (180, 120))
 grass = pygame.transform.scale(grass, (1240, 620))
 hero = pygame.transform.scale(hero, (180, 120))
 nature = pygame.transform.scale(nature, (180, 160))
-show(nature,grass)
+bullet = pygame.transform.scale(bullet, (80, 20))
+naturerect=pygame.Rect(nature.get_rect())
+badrect = pygame.Rect(hero.get_rect())
+zombrect=pygame.Rect(player.get_rect())
+startgame=font.render("Click to start the game",True,(100,100,100),(200,200,200))
+bla=0
+while 1:
+    screen.blit(hero,(300,150))
+    screen.blit(player,(700,150))
+    screen.blit(text,(500,250))
+    screen.blit(startgame,(495,300))
+    startrect = pygame.Rect(startgame.get_rect())
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            exit(1)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
+            if 495<= mouse[0] <=495+startrect[2]  and 300<= mouse[1] <=300+startrect[3]:
+                bla=1
+    if bla==1: break
+bla=0
+count=0
+show(nature,grass,count)
 screen.blit(player,(750,300))
 screen.blit(hero,(250,0))
 while 1:
     pygame.display.flip()
+    if((int(time.time())-start_time)%1==0 and c!=int(time.time())):
+        enemypos.append([1100,random.randint(0,520)])
+        a=int(time.time())
+        timeline.append(a)
+        c=int(time.time())
+    show(nature, grass,count)
+    screen.blit(hero, pos)
+    for i in range(len(enemypos)):
+        if((int(time.time())-timeline[i])%1==0 and d!=int(time.time())):
+            enemypos[i][0]=enemypos[i][0]-40
+        if(i==len(enemypos)-1):
+            d = int(time.time())
+        screen.blit(player, enemypos[i])
+        if (enemypos[i][0] <= naturerect[2]):
+            bla = 1
+            break
+    if(bla==1): break
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
@@ -30,20 +91,32 @@ while 1:
             if event.key==K_DOWN or event.key==K_s:
                 if(pos[1]+50<=620):
                     pos[1]=pos[1]+50
-                show(nature,grass)
+                show(nature,grass,count)
                 screen.blit(hero, pos)
             if event.key==K_UP or event.key==K_w:
                 if(pos[1]-50>=0):
                     pos[1]=pos[1]-50
-                show(nature,grass)
+                show(nature,grass,count)
                 screen.blit(hero, pos)
             if event.key==K_a or event.key==K_LEFT:
                 if(pos[0]-50>=250):
                     pos[0]=pos[0]-50
-                show(nature,grass)
+                show(nature,grass,count)
                 screen.blit(hero, pos)
             if event.key==K_d or event.key==K_RIGHT:
                 if(pos[0]+50<=1240):
                     pos[0]=pos[0]+50
-                show(nature,grass)
+                show(nature,grass,count)
                 screen.blit(hero, pos)
+            if event.key==K_SPACE:
+                for i in range(len(enemypos)):
+                    if((badrect[3]//2)+pos[1]>=enemypos[i][1] and (badrect[3]//2)+pos[1]<=enemypos[i][1]+zombrect[3]):
+                        screen.blit(bullet,(pos[0]+badrect[2]+40,badrect[3]//2+pos[1]))
+                        screen.blit(player,enemypos[i])
+                        enemypos.pop(i)
+                        timeline.pop(i)
+                        count+=1
+                        break
+while 1:
+    pygame.draw.rect(screen, (0,255,0,128), pygame.Rect(0, 0, 1240, 620))
+    pygame.display.flip()
